edf288a4ec96832bf7758b98fd12c9fd3ed2f024
syrekable <jordan2000niedziela@gmail.com>
2020-01-20 14:29:04 +0100

    Gonna make a living outa this

---
+++ app.js
@@ -0,0 +1,9 @@
+message = {
+	1:"Whoa, that's",
+	2:"some really cool",
+	3:"object right there"
+}
+
+function jssucks(element){console.log(element)}
+
+message.forEach(jssucks)
--- readme.md
+++
@@ -1,2 +0,0 @@
-I can't really do this
-I don't know the markdown language anyways
